ab687bbfa76c8ab9d86d4e729c70be2783749c48
Sebastian Espinosa Hernandez <sebasespinosa1812@hotmail.com>
2023-09-16 18:18:02 -0500

    New abstract class Employee created

---
+++ EmployeeOOP/Classes/Employee.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeOOP.Classes
+{
+    public abstract class Employee
+    {
+        #region Properties
+        public int Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public Date? Birthdate { get; set; }
+        public Date? HiringDate { get; set; }
+        public bool? IsActive { get; set; }
+        #endregion
+
+        #region Methods
+
+        public abstract decimal GetValueToPay();
+
+        public override string ToString()
+        {
+            return $"INFORMACION EMPLEADO \n\t" +
+                $"ID: {Id}\n\t" +
+                $"Nombres : {FirstName}\n\t" +
+                $"Apellidos : {LastName}\n\t" +
+                $"Fecha de cumpleaños: {Birthdate}\n\t" +
+                $"¿Empleado activo?: {IsActive}\n\t";
+        }
+
+        #endregion
+
+    }
+}
